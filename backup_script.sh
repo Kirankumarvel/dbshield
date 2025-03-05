b0370fae318d8487d1d642e6e8406a8cbfcf02d4
Kiran Kumar V <imkirankumar23@gmail.com>
2025-03-05 11:46:02 +0530

    Create backup_script.sh

---
+++ backup_script.sh
@@ -0,0 +1,8 @@
+DB_TYPE="mysql"   # Change to 'postgres' for PostgreSQL
+DB_NAME="your_database"
+DB_USER="your_username"
+DB_PASSWORD="your_password"
+BACKUP_DIR="/backups"
+UPLOAD_TO="s3"  # Change to 'gdrive' for Google Drive
+S3_BUCKET="s3://your-bucket-name"
+GDRIVE_FOLDER_ID="your-google-drive-folder-id"
